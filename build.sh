d50c1a9e8b68c9164d2c32addcb1e456b97e6291
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2024-02-11 15:26:56 -0300

    Build script formatting

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Abort on failure
+set -e
+
 ### Functions ###
 
 # Announces something on the terminal
@@ -27,7 +30,7 @@ build_akko()
     fi
 
     printf '\n> Building Akko for %s\n' "$2"
-    dotnet publish -c Release "$1" -r "$2" -p:PublishSingleFile=true --self-contained --nologo
+    dotnet publish --self-contained --nologo -c Release "$1" -r "$2" -p:PublishSingleFile=true
 }
 
 # Builds a cog
@@ -40,7 +43,7 @@ build_cog()
     fi
 
     printf '\n> Building cog "%s"\n' "$(basename "$1")"
-    dotnet publish -c Release "$1" --nologo
+    dotnet publish --nologo -c Release "$1"
 }
 
 # Cleans up a publish folder
@@ -54,7 +57,7 @@ cleanup_publish()
 
     # Check if glob patterns are not enabled
     if [[ $(shopt extglob) =~ "off" ]]; then
-        local glob='true'
+        local -r glob='true'
         shopt -s extglob
     fi
 
@@ -63,7 +66,7 @@ cleanup_publish()
 
     # Delete all compilation files except the binary we 
     # just moved and the Data folder
-    local pattern="$1/!(AkkoBot.exe|AkkoBot|Data)" # This variable is needed because Bash whines otherwise
+    local -r pattern="$1/!(AkkoBot.exe|AkkoBot|Data)" # This variable is needed because Bash whines otherwise
     rm -rf "$pattern"
 
     # Disable glob patterns if it was disabled previously
@@ -83,8 +86,8 @@ create_pack()
     fi
 
     # If function has 3 or less parameters or pack_tag is unset/empty
-    # set local variable to $3, else set to $4
-    local pack_name=$( (( $# <= 3 )) || [[ -z $4 ]] && echo "$3" || echo "$4" )
+    # set local -r variable to $3, else set to $4
+    local -r pack_name=$( (( $# <= 3 )) || [[ -z $4 ]] && echo "$3" || echo "$4" )
 
     # Create the .zip file
     # We need to juggle with pushd because zip likes to add junk
@@ -98,38 +101,35 @@ create_pack()
 ### Main ###
 
 # Variables
-declare -r BOT_VER=$(get_input 'Type the version of the bot (eg. 0.3.5-beta): ')
+BOT_VER=$(get_input 'Type the version of the bot (eg. 0.3.5-beta): '); declare -r BOT_VER
 declare -r AKKO_DIR='./AkkoBot'
-declare -r PUBLISH_DIR="$AKKO_DIR/bin/Release/net7.0"
+declare -r PUBLISH_DIR="${AKKO_DIR}/bin/Release/net7.0"
 declare -r COG_DIRS=('./AkkoCog.AntiPhishing' './AkkoCog.DangerousCommands')
 declare -r ARCHS=('win-x64' 'linux-x64' 'linux-arm64' 'osx-x64' 'osx-arm64')
 
 # Clean up the publish folder
 announce 'Cleaning up the publish folder'
-printf 'Removed %s.\n' "$(tree $PUBLISH_DIR -a | tail -n 1)"
+[[ $(command -v tree) ]] || printf 'Removed %s.\n' "$(tree $PUBLISH_DIR -a | tail -n 1)"
 rm -rf $PUBLISH_DIR
 
 # Build the bot for multiple platforms
 announce 'Building Akko'
 
-for arch in "${ARCHS[@]}"
-do
+for arch in "${ARCHS[@]}"; do
     build_akko $AKKO_DIR "$arch"
 done
 
 # Build the cogs
 announce 'Building Cogs'
 
-for cog_dir in "${COG_DIRS[@]}"
-do
+for cog_dir in "${COG_DIRS[@]}"; do
     build_cog "$cog_dir"
 done
 
 # Clean up the publish folders
 announce 'Preparing the publish packages'
 
-for arch in "${ARCHS[@]}"
-do
+for arch in "${ARCHS[@]}"; do
     cleanup_publish "$PUBLISH_DIR/$arch"
     create_pack "$BOT_VER" $PUBLISH_DIR "$arch"
     echo -e "- Packaged $arch"
